f8f791258dd78c26c1cdf8d1fceb40041438850f
kamaljavarnd <kamal.javarndcorp@gmail.com>
2022-01-03 09:24:13 -0500

    Create API Created

--- app/models/index.js
+++ app/models/index.js
@@ -6,6 +6,6 @@ mongoose.Promise = global.Promise;
 const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
-db.tutorials = require("./books.model.js")(mongoose);
+db.Books = require("./books.model.js")(mongoose);
 
 module.exports = db;
